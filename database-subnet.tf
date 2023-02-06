5eef56c3129473d040622eb9b6ff197138e8dc0b
Jean <kenmogne12@gmail.com>
2023-02-06 12:27:40 -0600

    M-2-modified-database-subnet

--- database-subnet.tf
+++ database-subnet.tf
@@ -1,5 +1,6 @@
 resource "aws_subnet" "database-subnet-1" {
     cidr_block = "10.0.5.0/24"
+    vpc_id = aws_vpc.sample-enterprise-network.id
     availability_zone = "us-east-1c"
     tags = {
       name = "sample-enterprise-network-subnet-database-subnet-1"
