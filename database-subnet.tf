4d0b0a4ea6fddf850f4e0a0ebd2d7417fc303f44
Jean <kenmogne12@gmail.com>
2023-02-06 12:21:09 -0600

    added-database

---
+++ database-subnet.tf
@@ -0,0 +1,8 @@
+resource "aws_subnet" "database-subnet-private-1" {
+    cidr_block = "10.0.5.0/24"
+    availability_zone = "us-east-1c"
+    tags = {
+      name = "sample-enterprise-network-subnet-database-subnet-1"
+    }
+  
+}
