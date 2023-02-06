1943d245ae1156e7469cd7c4f0a402833003ce51
Jean <kenmogne12@gmail.com>
2023-02-06 11:18:59 -0600

    Added-2-public-subnet-into-configuration-file

---
+++ public-subnet.tf
@@ -0,0 +1,18 @@
+resource "aws_subnet" "public-subnet-1" {
+    cidr_block = "10.0.0.1/20"
+    vpc_id = aws_vpc.sample-enterprise-network.id
+    availability_zone = "us-east-1a"
+    tags = {
+        name = "sample-enterprise-network-public-subnet-1"
+    }
+  
+}
+resource "aws_subnet" "public-subnet-2" {
+    cidr_block = "10.0.0.2/20"
+    vpc_id = aws_vpc.sample-enterprise-network.id
+    availability_zone = "us-east-1b"
+    tags = {
+        name = "sample-enterprise-network-public-subnet-2"
+    }
+  
+}
